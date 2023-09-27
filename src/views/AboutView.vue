1fc31fa7df374da7b4961f1c36570a8ce118b809
Ilera22 <ilera.none@gmail.com>
2023-09-27 07:22:15 -0500

    primer push

---
+++ src/views/AboutView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div>
+    <h1>Lista de Posts</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <h2>{{ post.title }}</h2>
+        <div v-html="post.html"></div>
+      </li>
+    </ul>
+  </div>
+  </div>
+
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  mounted() {
+    // Realiza una solicitud GET a tu API de posts (ajusta la URL según tu configuración)
+    axios.get('https://v1.nocodeapi.com/ilera22/ghost/UvunKKyBrORNwJDY')
+      .then((response) => {
+        this.posts = response.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+};
+</script>
+
+<style>
+
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
